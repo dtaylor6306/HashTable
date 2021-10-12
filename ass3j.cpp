8a6dec9d089963ad1ca5c1acb9f707dbe301aa85
Dylan Taylor <dtaylor6306@gmail.com>
2021-10-12 09:49:11 -0600

    hash class added with insert and print

--- ass3j.cpp
+++ ass3j.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
-//#include "hashtable.h"
-#include <bits/stdc++.h>
+#include "hashClass.h"
+#include <list>
+#include <string>
 
 using namespace std;
 
 struct data{
     string word;
-    int count = 0;
+    int count = 1;
 };
 
 int main(){
-
+/*
     list <data> items[10];
 
     for(int i = 0; i < 10; i++){
@@ -19,17 +20,33 @@ int main(){
         d.word = "hello";
         items[i].push_back(d);
     }
+
     data k;
     k.count = 9;
     k.word = "another one";
     items[0].push_back(k);
-for(int i = 0; i< 10; i++){
+    
+for(int i = 0; i < 10; i++){
     std::list<data>::iterator it;
     for (it = items[i].begin(); it != items[i].end(); ++it){
         std::cout << it->word << " " << it->count << endl;
     }
 
 }
+ */
+
+    Hash hashTable(10);
+
+    hashTable.insert("abc");
+    hashTable.insert("abc");
+    hashTable.insert("abc");
+    hashTable.insert("bcd");
+    hashTable.insert("bcd");
+    hashTable.insert("cde");
+    hashTable.insert("def");
+    hashTable.insert("efg");
+    hashTable.insert("abbe");
+    hashTable.printHash();
 
     return 0;
 }
---
+++ hashClass.h
@@ -0,0 +1,72 @@
+#include <string>
+#include <list>
+#include <iostream>
+using namespace std;
+class Hash{
+
+private:
+
+    int SIZE;
+
+    struct data{
+        int counter = 1;
+        string word;
+    };
+
+    list <data> *table;
+    int hash(string word){
+        int hashed = (int)word[0] % SIZE;
+
+        //cout << hashed << endl;
+
+        return hashed;
+    }
+
+public:
+    Hash(int newSIZE){
+        SIZE = newSIZE;
+        table = new list<data>[SIZE];
+    }
+
+    void insert(string newWord){
+        //[] hash to find pos
+        //[] if data in pos then counter ++
+        //[] else create new data and push back
+        int pos = hash(newWord);
+        
+        if(table[pos].empty()){
+            data d;
+            d.word = newWord;
+            table[pos].push_back(d);
+           // cout << "added " << newWord << endl;
+        }
+        else{
+            list<data>::iterator it;
+            for(it = table[pos].begin(); it != table[pos].end(); ++it){
+
+                if(it->word == newWord){
+                    it->counter++;
+                   // cout << "increased counter" << endl;
+                    break;
+                }
+            }
+            if(it == table[pos].end()){
+                data d;
+                d.word = newWord;
+                table[pos].push_back(d);
+
+                //cout << "added new word" << endl;
+            }
+    }
+        }
+
+    void printHash(){
+        for(int i = 0; i < SIZE; i++){
+            list<data>::iterator it;
+            for (it = table[i].begin(); it != table[i].end(); ++it){
+                cout << i << " " << it->word << " " << it->counter << endl;
+            }
+        }
+    }
+};
+
